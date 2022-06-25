eac2bb8e9bc92e19395582e531426d81ab671551
Kayleigh <102335456+cogillk@users.noreply.github.com>
2022-06-25 13:31:59 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LogIn from './components/Login-Form';
+import Navigation from './components/navigation';
+
+
+function App() { 
+  return (
+    <>
+      <Navigation/>
+      <LogIn/>
+    </>
+  );
+}
+export default App;
